6e8767f1f924e099109e8f3828cd9052717e2a0c
Kehinde09 <odukoyakehinde09@gmail.com>
2025-10-16 14:48:18 +0100

    Update AuctionMintingPolicyProperties.hs

--- test/AuctionMintingPolicyProperties.hs
+++ test/AuctionMintingPolicyProperties.hs
@@ -1,13 +1,15 @@
 import Test.QuickCheck
 import AuctionMintingPolicy
 
+-- Property: The minting policy should mint exactly one token when used correctly
 property_oneTokenMinted :: PubKeyHash -> Property
-property_oneTokenMinted pkh = 
-    let redeemer = ()
-        context = mockMintingScriptContext
-    in auctionTypedMintingPolicy pkh redeemer context ==> 
-        mintedExactlyOneToken context
+property_oneTokenMinted pkh =
+  let
+    redeemer = ()
+    context  = mockMintingScriptContext pkh
+  in
+    auctionTypedMintingPolicy pkh redeemer context ==> mintedExactlyOneToken context
 
+-- Run QuickCheck test
 main :: IO ()
 main = quickCheck property_oneTokenMinted
-
