ec39e368f4f7b731d990510e3f71b26c0dae514b
Hassan Michael Junior <michaelhassanjr@gmail.com>
2025-10-17 15:03:58 +0100

    Update AuctionMintingPolicySpec.hs

--- test/AuctionMintingPolicySpec.hs
+++ test/AuctionMintingPolicySpec.hs
@@ -3,38 +3,40 @@
 module Main where
 
 import Test.Hspec
-import AuctionMintingPolicy
+import AuctionMintingPolicy (auctionTypedMintingPolicy) -- Assuming this is your minting policy
 import PlutusLedgerApi.V1.Crypto (PubKeyHash(..))
 import PlutusLedgerApi.V2.Contexts (ScriptContext(..))
 import qualified PlutusLedgerApi.V2 as Plutus
+import Data.String (IsString(fromString))
 
 -- | A simple mock of ScriptContext for testing
-mockMintingScriptContext :: ScriptContext
-mockMintingScriptContext =
+mockMintingScriptContext :: Plutus.CurrencySymbol -> Plutus.TokenName -> Integer -> ScriptContext
+mockMintingScriptContext cs tn amount =
     ScriptContext
         { Plutus.scriptContextTxInfo = Plutus.TxInfo
             { Plutus.txInfoInputs = []
             , Plutus.txInfoOutputs = []
             , Plutus.txInfoFee = mempty
-            , Plutus.txInfoMint = oneTokenValue
+            , Plutus.txInfoMint = Plutus.singleton cs tn amount
             , Plutus.txInfoDCert = []
             , Plutus.txInfoWdrl = mempty
             , Plutus.txInfoValidRange = Plutus.always
             , Plutus.txInfoSignatories = []
             , Plutus.txInfoRedeemers = mempty
             , Plutus.txInfoData = mempty
-            , Plutus.txInfoId = "testTx"
+            , Plutus.txInfoId = fromString "testTx"
             }
-        , Plutus.scriptContextPurpose = Plutus.Minting "testCurrencySymbol"
+        , Plutus.scriptContextPurpose = Plutus.Minting cs
         }
-  where
-    oneTokenValue = Plutus.singleton "testCurrencySymbol" "TestToken" 1
 
 main :: IO ()
 main = hspec $ do
     describe "Auction Minting Policy" $ do
         it "allows minting exactly one token" $ do
             let pkh = PubKeyHash "12345678"
+            let cs = "testCurrencySymbol"
+            let tn = "TestToken"
+            let amount = 1
             let redeemer = ()
-            let context = mockMintingScriptContext
-            auctionTypedMintingPolicy pkh redeemer context `shouldBe` True
+            let ctx = mockMintingScriptContext cs tn amount
+            auctionTypedMintingPolicy pkh redeemer ctx `shouldBe` True
