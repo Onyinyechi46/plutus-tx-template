6ada1b69e13ca40707fb0d9da64e32a8f9fbc342
Fred_webtech <fredrickblessed73@gmail.com>
2025-10-17 16:34:20 -0500

    Update AuctionValidatorSpec.hs

--- test/AuctionValidatorSpec.hs
+++ test/AuctionValidatorSpec.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Main (main) where
 
@@ -11,54 +11,70 @@ import           PlutusLedgerApi.V1             (Lovelace (..))
 import           PlutusLedgerApi.V1.Interval    (always)
 
 import           PlutusLedgerApi.V2             ( CurrencySymbol (..)
-                                               , TokenName      (..)
-                                               , ScriptContext  (..)
-                                               , TxInfo         (..)
+                                               , TokenName (..)
+                                               , ScriptContext (..)
+                                               , TxInfo (..)
                                                )
 import           PlutusLedgerApi.V2.Contexts    ( ScriptPurpose (..)
-                                               , TxOutRef       (..)
-                                               , TxId           (..)
-                                               , TxInInfo
+                                               , TxOutRef (..)
+                                               , TxId (..)
                                                )
 import qualified PlutusTx.AssocMap             as AssocMap
 
--- | A completely empty TxInfo / ScriptContext used only for unit tests.
+------------------------------------------------------------
+-- Mock Utilities
+------------------------------------------------------------
+
+-- | A minimal empty TxInfo, reusable for testing.
+emptyTxInfo :: TxInfo
+emptyTxInfo = TxInfo
+  { txInfoInputs          = []
+  , txInfoReferenceInputs = []
+  , txInfoOutputs         = []
+  , txInfoFee             = mempty
+  , txInfoMint            = mempty
+  , txInfoDCert           = []
+  , txInfoWdrl            = AssocMap.empty
+  , txInfoValidRange      = always
+  , txInfoSignatories     = []
+  , txInfoData            = AssocMap.empty
+  , txInfoId              = TxId ""
+  , txInfoRedeemers       = AssocMap.empty
+  }
+
+-- | A generic empty ScriptContext.
 mockScriptContext :: ScriptContext
-mockScriptContext =
-  ScriptContext
-    { scriptContextTxInfo =
-        TxInfo
-          { txInfoInputs           = []
-          , txInfoReferenceInputs  = []
-          , txInfoOutputs          = []
-          , txInfoFee              = mempty
-          , txInfoMint             = mempty
-          , txInfoDCert            = []
-          , txInfoWdrl             = AssocMap.empty
-          , txInfoValidRange       = always
-          , txInfoSignatories      = []
-          , txInfoData             = AssocMap.empty
-          , txInfoId               = TxId ""
-          , txInfoRedeemers        = AssocMap.empty
-          }
-    , scriptContextPurpose = Spending (TxOutRef (TxId "") 0)
-    }
+mockScriptContext = ScriptContext
+  { scriptContextTxInfo = emptyTxInfo
+  , scriptContextPurpose = Spending (TxOutRef (TxId "") 0)
+  }
+
+------------------------------------------------------------
+-- Sample Parameters for Reuse
+------------------------------------------------------------
+
+defaultAuctionParams :: AuctionParams
+defaultAuctionParams = AuctionParams
+  { apSeller         = PubKeyHash "12345678"
+  , apCurrencySymbol = CurrencySymbol ""
+  , apTokenName      = TokenName "MY_TOKEN"
+  , apMinBid         = Lovelace 100
+  , apEndTime        = 1725227091000
+  }
+
+------------------------------------------------------------
+-- Tests
+------------------------------------------------------------
 
 main :: IO ()
 main = hspec $ do
-  describe "auctionTypedValidator" $ do
-    it "rejects a new bid when the context has no outputs" $ do
-      let params = AuctionParams
-            { apSeller         = PubKeyHash "12345678"
-            , apCurrencySymbol = CurrencySymbol ""    -- dummy
-            , apTokenName      = TokenName "MY_TOKEN"
-            , apMinBid         = Lovelace 100
-            , apEndTime        = 1725227091000
-            }
-          previousBid = Just (Bid "addr" (PubKeyHash "oldBidder") (Lovelace 50))
-          newBid      = Bid   "addr" (PubKeyHash "newBidder") (Lovelace 150)
+  describe "Auction Validator" $ do
+
+    it "rejects a new bid when there are no outputs in the context" $ do
+      let previousBid = Just (Bid "addr" (PubKeyHash "oldBidder") (Lovelace 50))
+          newBid      = Bid "addr" (PubKeyHash "newBidder") (Lovelace 150)
           datum       = AuctionDatum previousBid
           redeemer    = NewBid newBid
 
-      auctionTypedValidator params datum redeemer mockScriptContext
+      auctionTypedValidator defaultAuctionParams datum redeemer mockScriptContext
         `shouldBe` False
