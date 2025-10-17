80c52741afd4a2f460fd29a04ae5f2961e1e37a2
Hassan Michael Junior <michaelhassanjr@gmail.com>
2025-10-17 12:56:39 +0100

    Update AuctionMintingPolicySpec.hs

--- test/AuctionMintingPolicySpec.hs
+++ test/AuctionMintingPolicySpec.hs
@@ -1,13 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
 import Test.Hspec
 import AuctionMintingPolicy
 import PlutusLedgerApi.V1.Crypto (PubKeyHash(..))
+import PlutusLedgerApi.V2.Contexts (ScriptContext(..))
+import qualified PlutusLedgerApi.V2 as Plutus
+
+-- | A simple mock of ScriptContext for testing
+mockMintingScriptContext :: ScriptContext
+mockMintingScriptContext =
+    ScriptContext
+        { Plutus.scriptContextTxInfo = Plutus.TxInfo
+            { Plutus.txInfoInputs = []
+            , Plutus.txInfoOutputs = []
+            , Plutus.txInfoFee = mempty
+            , Plutus.txInfoMint = oneTokenValue
+            , Plutus.txInfoDCert = []
+            , Plutus.txInfoWdrl = mempty
+            , Plutus.txInfoValidRange = Plutus.always
+            , Plutus.txInfoSignatories = []
+            , Plutus.txInfoRedeemers = mempty
+            , Plutus.txInfoData = mempty
+            , Plutus.txInfoId = "testTx"
+            }
+        , Plutus.scriptContextPurpose = Plutus.Minting "testCurrencySymbol"
+        }
+  where
+    oneTokenValue = Plutus.singleton "testCurrencySymbol" "TestToken" 1
 
 main :: IO ()
 main = hspec $ do
     describe "Auction Minting Policy" $ do
-        it "should allow minting exactly one token" $ do
+        it "allows minting exactly one token" $ do
             let pkh = PubKeyHash "12345678"
             let redeemer = ()
-            let context = mockMintingScriptContext -- Define a mock ScriptContext
+            let context = mockMintingScriptContext
             auctionTypedMintingPolicy pkh redeemer context `shouldBe` True
-
