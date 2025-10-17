ac4df242f21eb472c81796c9ba67e8e5c0cab2b0
Hassan Michael Junior <michaelhassanjr@gmail.com>
2025-10-17 12:59:03 +0100

    Update AuctionValidatorProperties.hs

--- test/AuctionValidatorProperties.hs
+++ test/AuctionValidatorProperties.hs
@@ -3,6 +3,7 @@
 import Test.QuickCheck
 import AuctionValidator
 
+-- | Arbitrary instance for Bid
 instance Arbitrary Bid where
     arbitrary = do
         addr <- arbitrary
@@ -10,15 +11,36 @@ instance Arbitrary Bid where
         amt <- arbitrary `suchThat` (> 0)  -- Ensure the bid amount is positive
         return $ Bid addr pkh amt
 
+-- | Mock AuctionParams for tests
+mockAuctionParams :: AuctionParams
+mockAuctionParams = AuctionParams
+    { aSeller        = "seller"
+    , aCurrencySymbol = "currencySymbol"
+    , aTokenName     = "MY_TOKEN"
+    , aMinBid        = 100
+    , aDeadline      = 1725227091000
+    }
+
+-- | Property: new bid must be higher than the previous bid
 property_newBidHigherThanPrevious :: Bid -> Bid -> Property
-property_newBidHigherThanPrevious previousBid newBid = 
-    let params = AuctionParams "seller" "currencySymbol" "MY_TOKEN" 100 1725227091000
-        datum = AuctionDatum (Just previousBid)
+property_newBidHigherThanPrevious previousBid newBid =
+    let datum    = AuctionDatum (Just previousBid)
         redeemer = NewBid newBid
-        context = mockScriptContext
-    in (bAmount newBid > bAmount previousBid) ==> 
-        auctionTypedValidator params datum redeemer context
+        context  = mockScriptContext
+    in (bAmount newBid > bAmount previousBid) ==>
+        auctionTypedValidator mockAuctionParams datum redeemer context
+
+-- | Property: bids lower or equal to the previous should fail
+property_lowerBidFails :: Bid -> Bid -> Property
+property_lowerBidFails previousBid newBid =
+    let datum    = AuctionDatum (Just previousBid)
+        redeemer = NewBid newBid
+        context  = mockScriptContext
+    in (bAmount newBid <= bAmount previousBid) ==>
+        not (auctionTypedValidator mockAuctionParams datum redeemer context)
 
 main :: IO ()
-main = quickCheck property_newBidHigherThanPrevious
-
+main = do
+    putStrLn "\n=== Auction Validator Property Tests ==="
+    quickCheck property_newBidHigherThanPrevious
+    quickCheck property_lowerBidFails
