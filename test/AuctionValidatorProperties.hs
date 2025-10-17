45d2d9c8a3a9fec965996267d91eec0139e2bc52
Hassan Michael Junior <michaelhassanjr@gmail.com>
2025-10-17 15:06:10 +0100

    Update AuctionValidatorProperties.hs

--- test/AuctionValidatorProperties.hs
+++ test/AuctionValidatorProperties.hs
@@ -3,15 +3,15 @@
 import Test.QuickCheck
 import AuctionValidator
 
--- | Arbitrary instance for Bid
+-- Arbitrary instance for Bid with positive amounts
 instance Arbitrary Bid where
     arbitrary = do
         addr <- arbitrary
         pkh <- arbitrary
-        amt <- arbitrary `suchThat` (> 0)  -- Ensure the bid amount is positive
+        amt <- arbitrary `suchThat` (> 0)  -- Positive bid amount
         return $ Bid addr pkh amt
 
--- | Mock AuctionParams for tests
+-- Mock AuctionParams for testing
 mockAuctionParams :: AuctionParams
 mockAuctionParams = AuctionParams
     { aSeller        = "seller"
@@ -21,26 +21,44 @@ mockAuctionParams = AuctionParams
     , aDeadline      = 1725227091000
     }
 
--- | Property: new bid must be higher than the previous bid
-property_newBidHigherThanPrevious :: Bid -> Bid -> Property
-property_newBidHigherThanPrevious previousBid newBid =
-    let datum    = AuctionDatum (Just previousBid)
-        redeemer = NewBid newBid
-        context  = mockScriptContext
-    in (bAmount newBid > bAmount previousBid) ==>
-        auctionTypedValidator mockAuctionParams datum redeemer context
+-- Mock ScriptContext placeholder
+mockScriptContext :: ScriptContext
+mockScriptContext = undefined  -- You need a real or mocked ScriptContext here
 
--- | Property: bids lower or equal to the previous should fail
-property_lowerBidFails :: Bid -> Bid -> Property
-property_lowerBidFails previousBid newBid =
-    let datum    = AuctionDatum (Just previousBid)
-        redeemer = NewBid newBid
-        context  = mockScriptContext
-    in (bAmount newBid <= bAmount previousBid) ==>
-        not (auctionTypedValidator mockAuctionParams datum redeemer context)
+-- Property: New bid must be strictly higher than the previous bid
+property_newBidHigherThanPrevious :: Property
+property_newBidHigherThanPrevious = 
+    forAll arbitrary $ \previousBid ->
+    forAll arbitrary $ \newBid ->
+    (bAmount newBid > bAmount previousBid) ==>
+        let datum    = AuctionDatum (Just previousBid)
+            redeemer = NewBid newBid
+            context  = mockScriptContext
+            result   = auctionTypedValidator mockAuctionParams datum redeemer context
+        in counterexample
+            ("Previous bid: " ++ show (bAmount previousBid) ++
+             ", New bid: " ++ show (bAmount newBid) ++
+             ", Validator result: " ++ show result)
+            result
+
+-- Property: Bids that are lower or equal to the previous bid should fail validation
+property_lowerOrEqualBidFails :: Property
+property_lowerOrEqualBidFails = 
+    forAll arbitrary $ \previousBid ->
+    forAll arbitrary $ \newBid ->
+    (bAmount newBid <= bAmount previousBid) ==>
+        let datum    = AuctionDatum (Just previousBid)
+            redeemer = NewBid newBid
+            context  = mockScriptContext
+            result   = auctionTypedValidator mockAuctionParams datum redeemer context
+        in counterexample
+            ("Previous bid: " ++ show (bAmount previousBid) ++
+             ", New bid: " ++ show (bAmount newBid) ++
+             ", Validator result: " ++ show result)
+            (not result)
 
 main :: IO ()
 main = do
     putStrLn "\n=== Auction Validator Property Tests ==="
-    quickCheck property_newBidHigherThanPrevious
-    quickCheck property_lowerBidFails
+    quickCheck $ label "New bids higher than previous should validate" property_newBidHigherThanPrevious
+    quickCheck $ label "Bids lower or equal to previous should fail" property_lowerOrEqualBidFails
