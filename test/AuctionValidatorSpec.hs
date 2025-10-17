43001dddb1a78d8406f26224192342ef35466025
Hassan Michael Junior <michaelhassanjr@gmail.com>
2025-10-17 13:50:56 +0100

    Update AuctionValidatorSpec.hs

--- test/AuctionValidatorSpec.hs
+++ test/AuctionValidatorSpec.hs
@@ -18,47 +18,71 @@ import           PlutusLedgerApi.V2             ( CurrencySymbol (..)
 import           PlutusLedgerApi.V2.Contexts    ( ScriptPurpose (..)
                                                , TxOutRef       (..)
                                                , TxId           (..)
-                                               , TxInInfo
                                                )
 import qualified PlutusTx.AssocMap             as AssocMap
 
--- | A completely empty TxInfo / ScriptContext used only for unit tests.
+-- | Minimal mock ScriptContext for unit testing.
 mockScriptContext :: ScriptContext
 mockScriptContext =
   ScriptContext
     { scriptContextTxInfo =
         TxInfo
-          { txInfoInputs           = []
-          , txInfoReferenceInputs  = []
-          , txInfoOutputs          = []
-          , txInfoFee              = mempty
-          , txInfoMint             = mempty
-          , txInfoDCert            = []
-          , txInfoWdrl             = AssocMap.empty
-          , txInfoValidRange       = always
-          , txInfoSignatories      = []
-          , txInfoData             = AssocMap.empty
-          , txInfoId               = TxId ""
-          , txInfoRedeemers        = AssocMap.empty
+          { txInfoInputs          = []
+          , txInfoReferenceInputs = []
+          , txInfoOutputs         = []
+          , txInfoFee             = mempty
+          , txInfoMint            = mempty
+          , txInfoDCert           = []
+          , txInfoWdrl            = AssocMap.empty
+          , txInfoValidRange      = always
+          , txInfoSignatories     = []
+          , txInfoData            = AssocMap.empty
+          , txInfoId              = TxId ""
+          , txInfoRedeemers       = AssocMap.empty
           }
     , scriptContextPurpose = Spending (TxOutRef (TxId "") 0)
     }
 
+-- | Helper: construct dummy AuctionParams quickly
+mkParams :: PubKeyHash -> Lovelace -> AuctionParams
+mkParams seller minBid =
+  AuctionParams
+    { apSeller         = seller
+    , apCurrencySymbol = CurrencySymbol ""
+    , apTokenName      = TokenName "MY_TOKEN"
+    , apMinBid         = minBid
+    , apEndTime        = 1725227091000
+    }
+
+-- | Dummy bids for test readability
+oldBidder, newBidder :: PubKeyHash
+oldBidder = PubKeyHash "oldBidder"
+newBidder = PubKeyHash "newBidder"
+
+-- | Main test suite
 main :: IO ()
 main = hspec $ do
   describe "auctionTypedValidator" $ do
+
     it "rejects a new bid when the context has no outputs" $ do
-      let params = AuctionParams
-            { apSeller         = PubKeyHash "12345678"
-            , apCurrencySymbol = CurrencySymbol ""    -- dummy
-            , apTokenName      = TokenName "MY_TOKEN"
-            , apMinBid         = Lovelace 100
-            , apEndTime        = 1725227091000
-            }
-          previousBid = Just (Bid "addr" (PubKeyHash "oldBidder") (Lovelace 50))
-          newBid      = Bid   "addr" (PubKeyHash "newBidder") (Lovelace 150)
+      let params      = mkParams (PubKeyHash "12345678") (Lovelace 100)
+          previousBid = Just (Bid "addr" oldBidder (Lovelace 50))
+          newBid      = Bid   "addr" newBidder (Lovelace 150)
           datum       = AuctionDatum previousBid
           redeemer    = NewBid newBid
+          ctx         = mockScriptContext
 
-      auctionTypedValidator params datum redeemer mockScriptContext
+      auctionTypedValidator params datum redeemer ctx
         `shouldBe` False
+
+    it "accepts a higher bid when context is valid (placeholder test)" $ do
+      let params      = mkParams (PubKeyHash "12345678") (Lovelace 100)
+          previousBid = Just (Bid "addr" oldBidder (Lovelace 100))
+          newBid      = Bid   "addr" newBidder (Lovelace 200)
+          datum       = AuctionDatum previousBid
+          redeemer    = NewBid newBid
+          ctx         = mockScriptContext { scriptContextTxInfo = (scriptContextTxInfo mockScriptContext) { txInfoOutputs = [] } }
+          -- TODO: Replace [] with a valid TxOut simulating the new bidder’s output
+
+      auctionTypedValidator params datum redeemer ctx
+        `shouldBe` False  -- Change to True when TxOuts are mocked properly
